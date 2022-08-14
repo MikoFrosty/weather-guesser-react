eabaf4eb5b6a71fecbc27b2d3c3b521422b11890
brandon <srlguitarist@gmail.com>
2022-08-14 13:07:35 -0400

    Add props for city

--- src/components/GuessForm.js
+++ src/components/GuessForm.js
@@ -1,23 +1,28 @@
 import "./GuessForm.css";
 
-export default function GuessForm() {
+export default function GuessForm({ currentCity }) {
   return (
-    <form id="weather-guesser">
-      <label htmlFor="guess">
-        Enter Guess (<span id="temp-setting">℉</span>)
-      </label>
-      <input
-        type="number"
-        inputmode="numeric"
-        pattern="[0-9]*"
-        id="guess"
-        name="guess"
-        placeholder="0"
-        min="-150"
-        max="150"
-        required
-      />
-      <input type="submit" id="submit" class="button" value="Submit" />
-    </form>
+    <>
+      <h2>
+        How hot is <span id="current-city">{currentCity}</span> right now?
+      </h2>
+      <form id="weather-guesser">
+        <label htmlFor="guess">
+          Enter Guess (<span id="temp-setting">℉</span>)
+        </label>
+        <input
+          type="number"
+          inputmode="numeric"
+          pattern="[0-9]*"
+          id="guess"
+          name="guess"
+          placeholder="0"
+          min="-150"
+          max="150"
+          required
+        />
+        <input type="submit" id="submit" className="button" value="Submit" />
+      </form>
+    </>
   );
 }
