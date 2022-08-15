9f41444c8c2ff5b25f6c2ff25c3f7a8146a7da2a
brandon <srlguitarist@gmail.com>
2022-08-15 17:31:55 -0400

    Add core game logic, states, & imports

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ import NewCityAndOptions from "./components/NewCityAndOptions";
 import GuessForm from "./components/GuessForm";
 import MapAndOptions from "./components/MapAndOptions";
 import { places } from "./modules/places";
+import FetchWrapper from "./modules/fetchwrapper";
+import Answer from "./components/Answer";
 
 function App() {
   const [location, setLocation] = useState(["Loading...", ""]);
+  const [answer, setAnswer] = useState("");
+  const [weather, setWeather] = useState("");
+  const [options, setOptions] = useState({
+    temp: {
+      setting: 1, // 1 - f, 0 - c
+      display: "℉",
+    },
+    region: 0, // 0 - Global, 1 - North America, 2 - Europe, 3 - Asia
+    difficulty: 0, // 0 - Easy, 1 - Medium, 2 - Hard
+  });
 
   const [city, country] = location;
   const cityString = `${city
@@ -16,10 +28,12 @@ function App() {
     .replace(/[,]%20\w+/, "")}, ${country}`;
 
   useEffect(() => {
-    setLocation(() => places[Math.floor(Math.random() * places.length)]);
+    handleNewCityClick();
   }, []);
 
   function handleNewCityClick() {
+    setAnswer("");
+    setWeather("");
     setLocation(() => places[Math.floor(Math.random() * places.length)]);
   }
 
@@ -29,7 +43,72 @@ function App() {
 
   function handleFormSubmit(e) {
     e.preventDefault();
-    console.log(e.target.guess.value);
+    const guess = Number.parseInt(e.target.guess.value, 10);
+
+    const API = new FetchWrapper(
+      "https://api.weatherapi.com/v1/current.json?key=72b4230dbcfc4df5b07205043212612&q="
+    );
+
+    API.get(location[0])
+      .then((data) => {
+        let tempF = Math.floor(data.current.temp_f);
+        let tempC = Math.floor(data.current.temp_c);
+        let script = `The current temp in ${cityString} is ${
+          options.temp.setting ? tempF : tempC
+        }${options.temp.display}.`;
+        // Remove loader ?
+
+        // Handles winning, losing, and displaying proper messages
+        const runLogic = (temp) => {
+          // Expand win conditions based on difficulty
+          const tempRange = () => {
+            switch (options.difficulty) {
+              case 0:
+                return 10;
+              case 1:
+                return 5;
+              case 2:
+                return 0;
+              default:
+                return 10;
+            }
+          };
+
+          // Winning and losing messages
+          if (guess > temp + tempRange()) {
+            setAnswer(<><span className="loseh-text">Too high!</span> {script}</>);
+          } else if (guess < temp - tempRange()) {
+            setAnswer(<><span className="losel-text">Too low!</span> {script}</>);
+          } else {
+            setAnswer(<><span className="win-text">YOU WIN!</span> {script}</>);
+          }
+
+          // Display current weather
+          setWeather(
+            <>
+              <p>Current weather: {data.current.condition.text}</p>
+              <img src={data.current.condition.icon} alt={data.current.condition.text}/>
+            </>
+          );
+        };
+
+        // Run logic using Fahrenheit or Celsius
+        if (options.temp.setting) {
+          runLogic(tempF);
+        } else {
+          runLogic(tempC);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // remove loader ?
+        // answer message
+        //answer.textContent = "The weather is unavailable right now :/";
+      })
+      .finally(() => {
+        //enable submit button
+        //guessButton.removeAttribute("disabled");
+      });
   }
 
   return (
@@ -45,9 +124,8 @@ function App() {
               <span id="win-condition">within 10°</span>
             </sub>
           </p>
-          <p id="answer"></p>
-          <div id="current-weather"></div>
-          <MapAndOptions />
+          <Answer answer={answer} weather={weather}/>
+          <MapAndOptions location={location} />
         </section>
       </div>
       <Footer />
