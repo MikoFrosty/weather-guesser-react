f91379b67297d6fb9b8af1f73f1493197caa8f9f
brandon <srlguitarist@gmail.com>
2022-08-16 00:06:41 -0400

    Add function for different answer types

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,10 +1,48 @@
 import "./Answer.css";
 
-export default function Answer({ answer, weather }) {
+export default function Answer({ answer, weather, isLoading }) {
+  function getAnswer() {
+    const [condition, script] = answer;
+
+    switch (condition) {
+      case 0:
+        return "";
+      case 1:
+        return (
+          <>
+            <span className="loseh-text">Too high!</span> {script}
+          </>
+        );
+      case 2:
+        return (
+          <>
+            <span className="losel-text">Too low!</span> {script}
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <span className="win-text">YOU WIN!</span> {script}
+          </>
+        );
+      case 4:
+        return <code>The weather is unavailable right now :/</code>;
+      default:
+        return "ERROR";
+    }
+  }
+
   return (
     <>
-      <p id="answer">{answer}</p>
-      <div id="current-weather" style={{ borderTop: weather ? "1px solid black" : "none" }}>{weather}</div>
+      <p id="answer" className={isLoading ? "loader" : undefined}>
+        {getAnswer()}
+      </p>
+      <div
+        id="current-weather"
+        style={{ borderTop: weather ? "1px solid black" : "none" }}
+      >
+        {weather}
+      </div>
     </>
   );
 }
