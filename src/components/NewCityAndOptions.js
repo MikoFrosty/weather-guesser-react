81be25588db336e439d0bbb3eafa429706f3c046
brandon <srlguitarist@gmail.com>
2022-08-15 20:40:38 -0400

    Add onClick prop

--- src/components/NewCityAndOptions.js
+++ src/components/NewCityAndOptions.js
@@ -1,10 +1,10 @@
 import "./NewCityAndOptions.css";
 
-export default function NewCityAndOptions({ onNewCityClick }) {
+export default function NewCityAndOptions({ onNewCityClick, onOptionsClick }) {
     return (
         <div id="button-wrapper">
           <button id="new-city-button" className="button" onClick={onNewCityClick}>New City</button>
-          <button id="option-button" className="button">Options</button>
+          <button id="option-button" className="button" onClick={onOptionsClick}>Options</button>
         </div>
     );
 }
